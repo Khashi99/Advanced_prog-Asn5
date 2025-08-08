73f063aae7648052233d5a32ca93d7f79099bca9
Khashayar Zardoui <161512870+Khashi99@users.noreply.github.com>
2025-08-08 17:07:50 -0400

    Starting Asn5

---
+++ assignment5.h
@@ -0,0 +1,26 @@
+#ifndef ASSIGNMENT5_H_
+#define ASSIGNMENT5_H_
+
+#include <map>
+#include <string>
+#include <vector>
+#include <list>
+#include <algorithm>
+// Type aliases
+using WordVec = std::vector<std::string>;
+using WordList = std::list<std::string>;
+using WordMap = std::map<std::string, size_t>;
+
+WordVec read_words_into_vector(const std::string& inFileName);
+WordMap map_and_count_words_using_lambda(const WordVec& wvec);
+WordMap map_and_count_words_using_functor(const WordVec& wvec);
+WordVec remove_duplicates(const WordVec& words_vector);
+bool is_palindrome(const std::string& phrase);
+size_t count_using_lambda(const WordVec& vec, int n);
+size_t count_using_Free_Func(const WordVec& vec, int n);
+size_t count_using_Functor(const WordVec& vec, int n);
+void multisetUsingMyComparator(const WordVec& vec);
+std::vector<long> getnerate_Fibonacci(int n);
+bool areAnagrams(const std::string& s1, const std::string& s2);
+#include "Solutions.h"
+#endif
